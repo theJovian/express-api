f476c04c66f662715c9969404f1a4edf13945b38
Pablo Albito <pabloat98@outlook.es>
2022-04-14 11:59:34 +0200

    fixed iterations endpoint

--- routes/Iterations/iteration.js
+++ routes/Iterations/iteration.js
@@ -1,7 +1,7 @@
 const iteration = (app, pool) => {
     app.get('/api/iterations/:id', async (request, response) => {
         const id = request.params.id;
-        await pool.query('SELECT * FROM iterations WHERE experienceId = ?', id, (error, result) => {
+        await pool.query('SELECT * FROM Iterations WHERE experienceId = ?', id, (error, result) => {
             if (error) throw error;
 
             response.send(result);
@@ -11,10 +11,10 @@ const iteration = (app, pool) => {
     app.get('/api/iteration/:id', async (request, response) => {
         const id = request.params.id;
 
-        await pool.query('SELECT * FROM iterations WHERE id = ?', id, async(error, iteration) => {
+        await pool.query('SELECT * FROM Iterations WHERE id = ?', id, async(error, iteration) => {
             if (error) throw error;
 
-            await pool.query('SELECT * FROM items WHERE id = ?', id, (error, items) => {
+            await pool.query('SELECT * FROM Items WHERE id = ?', id, (error, items) => {
                 if (error) throw error;
 
                 const result = {
@@ -27,7 +27,7 @@ const iteration = (app, pool) => {
     });
     // Add a new iteration
     app.post('/api/iteration', (request, response) => {
-        pool.query('INSERT INTO iterations SET ?', request.body, (error, result) => {
+        pool.query('INSERT INTO Iterations SET ?', request.body, (error, result) => {
             if (error) throw error;
 
             response.status(201).send(`iteration added with ID: ${result.insertId}`);
@@ -37,7 +37,7 @@ const iteration = (app, pool) => {
     app.put('/api/iteration/:id', (request, response) => {
         const id = request.params.id;
 
-        pool.query('UPDATE iterations SET ? WHERE id = ?', [request.body, id], (error, result) => {
+        pool.query('UPDATE Iterations SET ? WHERE id = ?', [request.body, id], (error, result) => {
             if (error) throw error;
 
             response.send('iteration updated successfully.');
@@ -47,7 +47,7 @@ const iteration = (app, pool) => {
     app.delete('/api/iteration/:id', (request, response) => {
         const id = request.params.id;
 
-        pool.query('DELETE FROM iterations WHERE id = ?', id, (error, result) => {
+        pool.query('DELETE FROM Iterations WHERE id = ?', id, (error, result) => {
             if (error) throw error;
 
             response.send('iteration deleted.');
@@ -56,7 +56,7 @@ const iteration = (app, pool) => {
 
     app.delete('/api/iterations', (request, response) => {
         const ids = request.body
-        pool.query('DELETE FROM iterations WHERE id in (?)', [ids.trash], (error, result) => {
+        pool.query('DELETE FROM Iterations WHERE id in (?)', [ids.trash], (error, result) => {
             if (error) throw error;
             if(result.affectedRows === 0){
                 response.send('iterations ID does not exists: ' + ids.trash)
